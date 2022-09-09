defdb0f1f2f0e9d56ea419ca5d74521637107e50
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2022-09-08 17:42:02 -0700

    Create 5.13.2.cpp

---
+++ Chapter 5/5.13.2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/**
+   Return true if all elements in col are the same.
+   @param board a 3 x 3 array of strings
+   @param col the column to check
+   @return true if all elements are the same
+*/
+
+bool check_col(string board[3][3], int col)
+{
+   return !col ;
+}
