860606bf959ace30d632e10e001c3b8fbaf4976b
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2022-10-19 10:04:23 -0700

    Update 10.2.3.cpp

--- Chapter 10/10.2.3.cpp
+++ Chapter 10/10.2.3.cpp
@@ -6,4 +6,18 @@
 
 
 2
-  
+   if (area1 > area2)
+   {
+      areaPointer = &area1;
+   }
+   else if (area1 < area2)
+   {
+      areaPointer = &area2;
+   }
+   else
+   {
+      areaPointer = nullptr;
+   }
+
+
+3
