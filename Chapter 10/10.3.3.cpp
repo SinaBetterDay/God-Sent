c241328592416cb283a678818c36787d99ef4866
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2022-10-19 11:03:25 -0700

    Create 10.3.3.cpp

---
+++ Chapter 10/10.3.3.cpp
@@ -0,0 +1,17 @@
+#questions might differ from mine
+
+1
+   myDonkey = new Donkey(ageValue,weightValue);
+
+
+2
+   Cone* myCone;
+  
+   cin >> radiusValue;
+   cin >> heightValue;
+  
+   myCone->radius = radiusValue;
+   myCone->height = heightValue; 
+
+
+3
