5e7def0f5455471f16fe2052a88f6b49eac7a56f
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2022-10-24 13:20:05 -0700

    Create 10.5.3.cpp

---
+++ Chapter 10/10.5.3.cpp
@@ -0,0 +1,12 @@
+//questions might not match mine
+
+1.
+   firstDuck = new DuckNode(ducklings1);
+   headObj->InsertAfter(firstDuck);
+   
+   secondDuck = new DuckNode(ducklings2);
+   firstDuck->InsertAfter(secondDuck);
+
+
+2.
+  
