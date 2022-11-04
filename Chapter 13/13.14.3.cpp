c4cd5db82d0e20d9337309d0ad6909d40fcdc5c0
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2022-11-03 20:18:38 -0700

    Create 13.14.3.cpp

---
+++ Chapter 13/13.14.3.cpp
@@ -0,0 +1,21 @@
+#include "maybe.h"
+Maybe::Maybe()
+{
+    value_ptr = nullptr;
+}
+
+Maybe::Maybe(int v)
+{
+   value_ptr = new Int;
+   *value_ptr = v;
+}
+
+bool Maybe::is_valid() const
+{
+   return value_ptr != nullptr;
+}
+
+int Maybe::get_value() const
+{
+   return *value_ptr;
+}
