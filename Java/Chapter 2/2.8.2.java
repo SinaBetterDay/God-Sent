bf1942177ff600a0427750833c8ab03e3ce756f0
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2024-01-20 21:39:16 -0800

    Create 2.8.2.java

---
+++ Java/Chapter 2/2.8.2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class OutputCombinations {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      char a;
+      char b;
+      char c;
+
+      a = scnr.next().charAt(0);
+      b = scnr.next().charAt(0);
+      c = scnr.next().charAt(0);
+
+      System.out.println(""+a+b+c+" "+a+c+b+" "+b+a+c+" "+b+c+a+" "+c+a+b+" "+c+b+a);
+
+   }
+}
