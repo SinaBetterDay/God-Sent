e4bdfca7816d30be3ac6248086cecdc544553c0d
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2024-01-26 17:46:55 -0800

    Create 3.1.2 lvl1.java

---
+++ Java/Chapter 3/3.1.2 lvl1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TicketCounter {
+   public static void main (String [] args) {
+      int awardPoints;
+      int userTickets;
+   
+      Scanner scnr = new Scanner(System.in);
+      userTickets = scnr.nextInt(); // Program will be tested with values: 6, 7, 8, 9.
+   
+      if (userTickets != 8)
+      {
+         awardPoints = 10;
+      }
+      else
+      {
+         awardPoints = userTickets;
+      }
+
+      System.out.println(awardPoints);
+   }
+}
