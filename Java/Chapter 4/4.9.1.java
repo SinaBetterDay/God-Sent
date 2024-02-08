6a5f8709c8fb956fd766c9813109f721582b227c
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2024-02-08 15:47:47 -0800

    Create 4.9.1.java

---
+++ Java/Chapter 4/4.9.1.java
@@ -0,0 +1,20 @@
+public class GrocerySorter {
+   public enum GroceryItem {GR_APPLES, GR_BANANAS, GR_JUICE, GR_WATER}
+
+   public static void main (String[] args) {
+      GroceryItem userItem;
+
+      /* Your code will be tested with GR_APPLES and other values for userItem */
+      userItem = GroceryItem.GR_APPLES;
+
+      if (userItem == GroceryItem.GR_APPLES || userItem == GroceryItem.GR_BANANAS)
+      {
+         System.out.println("Fruit");
+      }
+      else
+      {
+         System.out.println("Drink");
+      }
+
+   }
+}
