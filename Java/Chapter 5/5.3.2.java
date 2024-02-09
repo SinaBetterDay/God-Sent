6e9d116678478e60a9ae5042885e541c1d7a9273
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2024-02-08 21:11:36 -0800

    Create 5.3.2.java

---
+++ Java/Chapter 5/5.3.2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MthdStubsStatistics {
+
+   public static int getUserNum()
+   {
+      System.out.println("FIXME: Finish getUserNum()");
+      return -1;
+   }
+   
+   public static int computeAvg(int num1, int num2)
+   {
+      System.out.println("FIXME: Finish computeAvg()");
+      return -1;
+   }
+
+   public static void main(String [] args) {
+      int userNum1;
+      int userNum2;
+      int avgResult;
+
+      userNum1 = getUserNum();
+      userNum2 = getUserNum();
+
+      avgResult = computeAvg(userNum1, userNum2);
+
+      System.out.println("Avg: " + avgResult);
+   }
+}
