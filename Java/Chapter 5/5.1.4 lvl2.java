5e581a7170126a239be2b96b3b19969fb06d16d5
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2024-02-08 17:38:40 -0800

    WOOOOOOOOOOOOOOOOO

--- Java/Chapter 5/5.1.4 lvl2.java
+++ Java/Chapter 5/5.1.4 lvl2.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class NumberCalculator {
+   
+   public static double calcNum(double num1, double num2)
+   {
+      double ans;
+      ans = num1 - (7.5 * num2);
+      return ans;
+   }
+   
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      double numIn1;
+      double numIn2;
+      double result;
+      
+      numIn1 = scnr.nextDouble();
+      numIn2 = scnr.nextDouble();
+      
+      result = calcNum(numIn1, numIn2);
+      
+      System.out.printf("%.2f", result);
+   }
+}
