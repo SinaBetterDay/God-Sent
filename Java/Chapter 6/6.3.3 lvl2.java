8c8ee8857d48f713b95923595adc57ce99ad8238
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2024-02-13 11:51:30 -0800

    Create 6.3.3 lvl2.java

---
+++ Java/Chapter 6/6.3.3 lvl2.java
@@ -0,0 +1,19 @@
+public class PilotLicense {
+   private int id, fee;
+   
+   public int getId()  {
+      return id;
+   }
+   
+   public int getFee()  {
+      return fee;
+   }
+
+	public void setId(int newId) {
+		id = newId;
+	}
+
+	public void setFee(int newFee) {
+		fee = newFee;
+	}
+}
