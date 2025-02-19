dda6a321c04c9d56674b656ce91032d275eb30c1
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2025-02-18 20:04:52 -0800

    Add files via upload

--- C/Loops/lab3.c
+++ C/Loops/lab3.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+   int num = 0;
+
+   printf("Input a number: \n");
+   scanf("%i", &num);
+
+   for (int i=1; i<num; i++) {
+      if(i%2==1) {
+         printf("for %d\n", i);
+      }
+   }
+
+   int count = 1;
+   while(count<8 && count<=num) {
+      printf("while %d\n", count);
+      count++;
+   }
+
+   count = 1;
+   do {
+      printf("do while %d\n", count);
+      count++;
+   } while(count <= num);
+
+   return EXIT_SUCCESS;
+}
